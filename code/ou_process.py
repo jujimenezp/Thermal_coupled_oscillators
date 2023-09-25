0344d5b37a1484b866338d30659b9d37071f8698
jujimenezp <jujimenezp@unal.edu.co>
2023-09-25 00:00:09 -0500

    Testing standard deviation evolution

--- code/ou_process.py
+++ code/ou_process.py
@@ -9,75 +9,160 @@ mpl.rcParams['font.family'] = 'STIXGeneral'
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
+def gaussian(x, mean, amplitude, variance):
+    return amplitude * np.exp( - (x - mean)**2 / (2*variance))
+
 def expo(t, y0, gamma):
     return y0*np.exp(-gamma*t)
 
-fig, ax = plt.subplots(1,3, layout='tight')
-data = pd.read_csv('results/ou_process_r0.1.dat', delimiter='\t')
+def f_sigma(t, gamma, b):
+    return np.sqrt((b**2/gamma)*(1-np.exp(-2*gamma*t)))
 
-t = data['t']
-v = data['vx']
-popt, pcov = curve_fit(expo, t.truncate(after=252), v.truncate(after=252))
-print(r'gamma=0.1')
-print(popt)
-print(pcov)
+bins = int(sys.argv[1])
+fig, ax = plt.subplots(1,3, layout='tight')
+fig2, ax2 = plt.subplots(1,3, layout='tight')
+data = pd.read_csv('results/ou_process_r0.1.dat', header=None, delimiter='\t')
+
+t=data.iloc[:,0]
+avg = []
+sigma = []
+
+for i in range(len(t+1)):
+    vx = data.iloc[i,1:]
+    bin_heights, bin_borders = np.histogram(vx,bins)
+    bin_centers = bin_borders[:-1] +np.diff(bin_borders)/2
+    try:
+        popt, pcov = curve_fit(gaussian, bin_centers, bin_heights, [5,10,1])
+    except:
+        print(f'Not using gaussian for t={i} in gamma=0.1')
+        popt=[]; pcov=[]
+        popt.append(np.average(vx))
+        popt.append(np.std(vx))
+    avg.append(popt[0])
+    sigma.append(np.sqrt(popt[1]))
+
+parms, parms_cov = curve_fit(expo, t, avg)
+print('\ngamma=1.0')
+print(parms)
+print(parms_cov)
 t_fit = np.linspace(0,100,1000)
-v_fit = expo(t_fit,popt[0],popt[1])
+v_fit = expo(t_fit,parms[0],parms[1])
 
 ax[0].set_title(r'$\gamma=0.1$')
 ax[0].grid()
 ax[0].set_xlabel('Tiempo')
 ax[0].set_ylabel('Velocidad')
-ax[0].scatter(t, v, s=1, color='black')
+ax[0].scatter(t, avg, s=1, color='black', label='Datos')
 textstr=(r'$v=v_0 e^{-\gamma t}$'
          '\n'
          r'$\gamma = 0.1059\pm 0.0004$')
 ax[0].plot(t_fit,v_fit,color='red',label=textstr)
 ax[0].legend(loc='upper right', fontsize=8)
 
-data = pd.read_csv('results/ou_process_r0.5.dat', delimiter='\t')
-t = data['t']
-v = data['vx']
-popt, pcov = curve_fit(expo, t.truncate(after=152), v.truncate(after=152))
-print(r'gamma=0.5')
-print(popt)
-print(pcov)
+parms2, parms_cov2 = curve_fit(f_sigma, t, sigma)
+print(f'\n{parms2}')
+print(parms_cov2)
 t_fit = np.linspace(0,100,1000)
-v_fit = expo(t_fit,popt[0],popt[1])
+sigma_fit = f_sigma(t_fit,parms2[0], parms2[1])
+ax2[0].grid()
+ax2[0].scatter(t,sigma,s=1,color='black')
+ax2[0].plot(t_fit,sigma_fit)
+
+data = pd.read_csv('results/ou_process_r0.5.dat', delimiter='\t')
+
+t=data.iloc[:,0]
+avg = []
+sigma = []
+
+for i in range(len(t+1)):
+    vx = data.iloc[i,1:]
+    bin_heights, bin_borders = np.histogram(vx,bins)
+    bin_centers = bin_borders[:-1] +np.diff(bin_borders)/2
+    try:
+        popt, pcov = curve_fit(gaussian, bin_centers, bin_heights, [5,10,1])
+    except:
+        print(f'Not using gaussian for t={i} in gamma=0.5')
+        popt=[]; pcov=[]
+        popt.append(np.average(vx))
+        popt.append(np.std(vx))
+    avg.append(popt[0])
+    sigma.append(np.sqrt(popt[1]))
+
+parms, parms_cov = curve_fit(expo, t, avg)
+print('\ngamma=1.0')
+print(parms)
+print(parms_cov)
+t_fit = np.linspace(0,100,1000)
+v_fit = expo(t_fit,parms[0],parms[1])
 
 ax[1].set_title(r'$\gamma=0.5$')
 ax[1].grid()
 ax[1].set_xlabel('Tiempo')
 ax[1].set_ylabel('Velocidad')
 ax[1].set_xlim(0,40)
-ax[1].scatter(t, v, s=1, color='black')
+ax[1].scatter(t, avg, s=1, color='black', label='Datos')
 textstr=(r'$v=v_0 e^{-\gamma t}$'
          '\n'
          r'$\gamma = 0.504\pm 0.003$')
 ax[1].plot(t_fit,v_fit,color='red',label=textstr)
 ax[1].legend(loc='upper right', fontsize=8)
 
-data = pd.read_csv('results/ou_process_r1.dat', delimiter='\t')
-t = data['t']
-v = data['vx']
-popt, pcov = curve_fit(expo, t.truncate(after=62), v.truncate(after=62))
-print(r'gamma=1')
-print(popt)
-print(pcov)
+parms2, parms_cov2 = curve_fit(f_sigma, t, sigma)
+print(f'\n{parms2}')
+print(parms_cov2)
 t_fit = np.linspace(0,100,1000)
-v_fit = expo(t_fit,popt[0],popt[1])
+sigma_fit = f_sigma(t_fit,parms2[0], parms2[1])
+ax2[1].grid()
+ax2[1].scatter(t,sigma,s=1,color='black')
+ax2[1].plot(t_fit,sigma_fit)
+
+data = pd.read_csv('results/ou_process_r1.dat', delimiter='\t')
+
+t=data.iloc[:,0]
+avg = []
+sigma = []
+
+for i in range(len(t+1)):
+    vx = data.iloc[i,1:]
+    bin_heights, bin_borders = np.histogram(vx,bins)
+    bin_centers = bin_borders[:-1] +np.diff(bin_borders)/2
+    try:
+        popt, pcov = curve_fit(gaussian, bin_centers, bin_heights, [5,10,1])
+    except:
+        print(f'Not using gaussian for t={i} in gamma=1')
+        popt=[]; pcov=[]
+        popt.append(np.average(vx))
+        popt.append(np.std(vx))
+    avg.append(popt[0])
+    sigma.append(np.sqrt(popt[1]))
+
+parms, parms_cov = curve_fit(expo, t, avg)
+print('\ngamma=1.0')
+print(parms)
+print(parms_cov)
+t_fit = np.linspace(0,100,1000)
+v_fit = expo(t_fit,parms[0],parms[1])
 
 ax[2].set_title(r'$\gamma=1$')
 ax[2].grid()
 ax[2].set_xlabel('Tiempo')
 ax[2].set_ylabel('Velocidad')
 ax[2].set_xlim(0,15)
-ax[2].scatter(t, v, s=1, color='black')
+ax[2].scatter(t, avg, s=1, color='black', label='Datos')
 textstr=(r'$v=v_0 e^{-\gamma t}$'
          '\n'
          r'$\gamma = 1.009\pm 0.006$')
 ax[2].plot(t_fit,v_fit,color='red',label=textstr)
 ax[2].legend(loc='upper right', fontsize=8)
 
-#plt.show()
-plt.savefig('results/ou_process.pdf',format='pdf',bbox_inches='tight')
+parms2, parms_cov2 = curve_fit(f_sigma, t, sigma)
+print(f'\n{parms2}')
+print(parms_cov2)
+t_fit = np.linspace(0,100,1000)
+sigma_fit = f_sigma(t_fit,parms2[0], parms2[1])
+ax2[2].grid()
+ax2[2].scatter(t,sigma,s=1,color='black')
+ax2[2].plot(t_fit,sigma_fit)
+
+plt.show()
+#plt.savefig('results/vel_dist.pdf',format='pdf',bbox_inches='tight')
--- code/rand_lf.cpp
+++ code/rand_lf.cpp
@@ -3,10 +3,10 @@
 int main(int argc, char** argv){
     double t_end=std::stod(argv[3]), dt=std::stod(argv[2]);
     int t_imp=1;
-    double k=0.5, f=1-exp(-0.05), T=0.01;
+    double k=0.5, f=1-exp(-dt*0.5), T=0.01;
     // particle(double Ri, double xi, double yi, double vxi, double vyi, double Fxi, double Fyi)
     //particle b(3, 0, 0, 0, 0, 0, 0);
-    std::vector<particle> bs = {1, particle(3, 0, 0, 10, 0, 0, 0)};
+    std::vector<particle> bs = {100, particle(3, 0, 0, 10, 0, 0, 0)};
     double SEED=std::stoi(argv[1]);
     gsl_rng * r = gsl_rng_alloc(gsl_rng_mt19937);
     gsl_rng_set(r,SEED);
@@ -17,14 +17,14 @@ int main(int argc, char** argv){
     osc.initialize_v(bs);
 
     std::ofstream file("results/ou_process_r0.5.dat");
-    file <<"t" << "\tx" << "\tvx" <<"\n";
+    //file <<"t" << "\tx" << "\tvx" <<"\n";
     for(double t=0; t < t_end; t+=dt){
         //osc.update_F(bs,k);
         if(int(t/dt)%t_imp == 0){
             file << t;
             for(int i=0; i < bs.size(); i++){
-                //file << "\t" << bs[i].x;
-                file <<"\t"<< bs[i].x <<"\t"<< bs[i].vx;
+                file << "\t" << bs[i].vx;
+                //file <<"\t"<< bs[i].x <<"\t"<< bs[i].vx;
             }
         file << "\n";
         }
