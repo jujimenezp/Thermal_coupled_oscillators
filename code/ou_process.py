07094386c1f6b11714dd82a617f8c0ea38bbb45c
jujimenezp <jujimenezp@unal.edu.co>
2023-09-27 08:51:27 -0500

    .

--- code/ou_process.py
+++ code/ou_process.py
@@ -42,22 +42,22 @@ for i in range(len(t+1)):
     sigma.append(np.sqrt(popt[1]))
 
 parms, parms_cov = curve_fit(expo, t, avg)
-print('\ngamma=0.1')
+print('\ngamma=0.01')
 print(parms)
 print(parms_cov)
 t_fit = np.linspace(0,250,1000)
 v_fit = expo(t_fit,parms[0],parms[1])
 
-ax[0].set_title(r'$\gamma=0.1$')
+ax[0].set_title(r'$\gamma=0.01$')
 ax[0].grid()
-ax[0].set_xlabel('Tiempo')
-ax[0].set_ylabel('Velocidad')
+ax[0].set_xlabel('Time')
+ax[0].set_ylabel('Velocity')
 ax[0].scatter(t, avg, s=1, color='black', label='Datos')
-textstr=(r'$v=v_0 e^{-\gamma t}$'
+textstr=(r'$\sigma=\sqrt{k_B T(1-e^{-2\gamma t})}$'
          '\n'
-         r'$\gamma = 0.1059\pm 0.0004$')
+         r'$\gamma = 0.01021\pm 0.00001$')
 ax[0].plot(t_fit,v_fit,color='red',label=textstr)
-ax[0].legend(loc='upper right', fontsize=8)
+ax[0].legend(loc='upper right', fontsize=6)
 
 parms2, parms_cov2 = curve_fit(f_sigma, t, sigma)
 print(f'\n{parms2}')
@@ -65,8 +65,17 @@ print(parms_cov2)
 t_fit = np.linspace(0,250,1000)
 sigma_fit = f_sigma(t_fit,parms2[0], parms2[1])
 ax2[0].grid()
+ax2[0].set_title(r'$\gamma=0.01$')
+ax2[0].set_xlabel('Time')
+ax2[0].set_ylabel('Velocity Standard Deviation')
+textstr=(r'$\sigma_v=\sqrt{k_B T(1-e^{-2\gamma t})}$'
+         '\n'
+         r'$\gamma = 0.1005\pm 0.0001$'
+         '\n'
+         r'$k_B T = 4.0191 \pm 0.0009$')
 ax2[0].scatter(t,sigma,s=1,color='black')
-ax2[0].plot(t_fit,sigma_fit)
+ax2[0].plot(t_fit,sigma_fit,label=textstr)
+ax2[0].legend(loc='lower right', fontsize=7)
 
 
 
@@ -92,18 +101,18 @@ for i in range(len(t+1)):
     sigma.append(np.sqrt(popt[1]))
 
 parms, parms_cov = curve_fit(expo, t, avg)
-print('\ngamma=0.5')
+print('\ngamma=0.05')
 print(parms)
 print(parms_cov)
 t_fit = np.linspace(0,200,1000)
 v_fit = expo(t_fit,parms[0],parms[1])
 
-ax[1].set_title(r'$\gamma=0.5$')
+ax[1].set_title(r'$\gamma=0.05$')
 ax[1].grid()
-ax[1].set_xlabel('Tiempo')
-ax[1].set_ylabel('Velocidad')
-ax[1].set_xlim(0,40)
-ax[1].scatter(t, avg, s=1, color='black', label='Datos')
+ax[1].set_xlabel('Time')
+ax[1].set_ylabel('Velocity')
+#ax[1].set_xlim(0,40)
+ax[1].scatter(t, avg, s=1, color='black', label='Data')
 textstr=(r'$v=v_0 e^{-\gamma t}$'
          '\n'
          r'$\gamma = 0.504\pm 0.003$')
@@ -116,9 +125,17 @@ print(parms_cov2)
 t_fit = np.linspace(0,200,1000)
 sigma_fit = f_sigma(t_fit,parms2[0], parms2[1])
 ax2[1].grid()
+ax2[1].set_title(r'$\gamma=0.05$')
+ax2[1].set_xlabel('Time')
+ax2[1].set_ylabel('Velocity Standard Deviation')
+textstr=(r'$\sigma_v=\sqrt{k_B T(1-e^{-2\gamma t})}$'
+         '\n'
+         r'$\gamma = 0.05086\pm 0.00008$'
+         '\n'
+         r'$k_B T = 4.0182 \pm 0.0009$')
 ax2[1].scatter(t,sigma,s=1,color='black')
-ax2[1].plot(t_fit,sigma_fit)
-
+ax2[1].plot(t_fit,sigma_fit, label=textstr)
+ax2[1].legend(loc='lower right', fontsize=7)
 
 
 
@@ -143,18 +160,18 @@ for i in range(len(t+1)):
     sigma.append(np.sqrt(popt[1]))
 
 parms, parms_cov = curve_fit(expo, t, avg)
-print('\ngamma=1.0')
+print('\ngamma=0.1')
 print(parms)
 print(parms_cov)
 t_fit = np.linspace(0,100,1000)
 v_fit = expo(t_fit,parms[0],parms[1])
 
-ax[2].set_title(r'$\gamma=1$')
+ax[2].set_title(r'$\gamma=0.1$')
 ax[2].grid()
-ax[2].set_xlabel('Tiempo')
-ax[2].set_ylabel('Velocidad')
-ax[2].set_xlim(0,15)
-ax[2].scatter(t, avg, s=1, color='black', label='Datos')
+ax[2].set_xlabel('Time')
+ax[2].set_ylabel('Velocity')
+#ax[2].set_xlim(0,15)
+ax[2].scatter(t, avg, s=1, color='black', label='Data')
 textstr=(r'$v=v_0 e^{-\gamma t}$'
          '\n'
          r'$\gamma = 1.009\pm 0.006$')
@@ -167,8 +184,18 @@ print(parms_cov2)
 t_fit = np.linspace(0,100,1000)
 sigma_fit = f_sigma(t_fit,parms2[0], parms2[1])
 ax2[2].grid()
+ax2[2].set_title(r'$\gamma=0.1$')
+ax2[2].set_xlabel('Time')
+ax2[2].set_ylabel('Velocity Standard Deviation')
+textstr=(r'$\sigma_v=\sqrt{k_B T(1-e^{-2\gamma t})}$'
+         '\n'
+         r'$\gamma = 0.0987\pm 0.0002$'
+         '\n'
+         r'$k_B T = 4.019 \pm 0.001$')
 ax2[2].scatter(t,sigma,s=1,color='black')
-ax2[2].plot(t_fit,sigma_fit)
+ax2[2].plot(t_fit,sigma_fit,label=textstr)
+ax2[2].legend(loc='lower right', fontsize=7)
 
-plt.show()
-#plt.savefig('results/vel_dist.pdf',format='pdf',bbox_inches='tight')
+#plt.show()
+fig.savefig('results/vel_dist1.png',format='png',bbox_inches='tight',dpi=300)
+fig2.savefig('results/vel_dist2.png',format='png',bbox_inches='tight',dpi=300)
--- code/pos_dist.py
+++ code/pos_dist.py
@@ -13,21 +13,21 @@ from scipy.stats import linregress
 def gaussian(x, mean, amplitude, standard_deviation):
     return amplitude * np.exp( - (x - mean)**2 / (2*standard_deviation ** 2))
 
-data = pd.read_csv('results/oscillator.dat', header=None, delimiter='\t')
+data = pd.read_csv('results/oscillator2.dat', header=None, delimiter='\t')
 
 ts = []
 xs = []
-bins=50
-ies = np.arange(2500,10,-25)
+bins=75
+ies = np.arange(20000,10,-25)
 fig,ax = plt.subplots(1,2, layout='tight')
 cmap = mpl.colormaps.get_cmap('gist_rainbow_r')
-color_gradients = cmap(ies/2500)
+color_gradients = cmap(ies/20000)
 for i in ies:
     ts.append(data.iloc[i,0])
     x=data.iloc[i,1:]
     bin_heights, bin_borders, _ = ax[0].hist(x,bins,color=color_gradients[int(i/25-1)])
     bin_centers = bin_borders[:-1] + np.diff(bin_borders)/2
-    popt, pcov = curve_fit(gaussian, bin_centers, bin_heights)
+    popt, pcov = curve_fit(gaussian, bin_centers, bin_heights, [0, 200, 50])
     xs.append(np.power(popt[2],2))
 
 res = linregress(ts, xs)
@@ -36,21 +36,20 @@ print(f'intercept = {res.intercept} +/- {res.intercept_stderr}')
 print(f'R2 = {res.rvalue}')
 t = np.linspace(0,9990,10000)
 y = res.intercept + t*res.slope
-ax[0].set_title('Distribuciones de posición')
+ax[0].set_title('Position Distributions')
 
-ax[1].set_title(r'Varianza vs tiempo')
-ax[1].set_xlabel(r'Tiempo [u.a]')
-ax[1].set_ylabel(r'Varianza')
+ax[1].set_title(r'Variance vs Time')
+ax[1].set_xlabel(r'Time')
+ax[1].set_ylabel(r'Variance')
+ax[1].grid()
 textstr=(r'$\sigma^2=2D t+b$'
          '\n'
-         r'$D=0.302 \pm 0.001$'
+         r'$D=0.5013\pm 0.0006$'
          '\n'
-         r'$b=8\pm9$'
-         '\n'
-         r'$R^2=0.9997$')
+         r'$R^2=0.9995$')
 ax[1].plot(t,y, color='red', label=textstr)
+ax[1].scatter(ts, xs, s=1, color='black', label='Data')
 ax[1].legend(loc='upper left', fontsize=8)
-ax[1].scatter(ts, xs, s=1, color='black')
 
 #plt.show()
-plt.savefig('results/pos_dist.pdf',format='pdf',bbox_inches='tight')
+plt.savefig('results/pos_dist.png',format='png',bbox_inches='tight',dpi=300)
